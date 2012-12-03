098c348b5a3b84a74e1db296b5b7fa20a5afccf2
Ethan <jiajunw@cmu.edu>
2012-12-03 18:36:35 -0500

    update

--- src/edu/cmu/phoenix/changecounter/OutputHandler.java
+++ src/edu/cmu/phoenix/changecounter/OutputHandler.java
@@ -1,18 +1,44 @@
 package edu.cmu.phoenix.changecounter;
+import java.io.*;
+
+/****************************************
+ *
+ * OutputHandler class takes stream from program
+ * and print it out to file.
+ * @author Ethan Wang
+ *
+ ****************************************/
 
 public class OutputHandler {
-        private String lineText;
-        private int lineNumber;
-        private Type type;
-
+	private OutputStream oStream;
+	
         public OutputHandler() {
-
+		
         }
 
-        public void writeChangeListing() {
-        }
+        public void writeChangeListing(Program newCL) {
+                try {
 
-	public void writeAnnotatedSourceFile() {
+                } catch (IOException e) {
+
+                }
+	}
+
+	public void writeAnnotatedSourceFile(Program newSF) {
+		try {
+			
+		} catch (IOException e) {
+
+		} 
+	}
+
+	public boolean closeIO () {
+	        try {
+			.close();
+			return true;
+                } catch (IOException e) {
+			return false;
+                }
 	}
 }
 
