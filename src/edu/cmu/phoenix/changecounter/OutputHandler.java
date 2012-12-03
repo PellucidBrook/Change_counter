aac16fe5e43b7fd233f053b447b8f3cd804e0c56
Ethan <jiajunw@cmu.edu>
2012-12-03 17:29:58 -0500

    sd

---
+++ src/edu/cmu/phoenix/changecounter/OutputHandler.java
@@ -0,0 +1,18 @@
+package edu.cmu.phoenix.changecounter;
+
+public class OutputHandler {
+        private String lineText;
+        private int lineNumber;
+        private Type type;
+
+        public OutputHandler() {
+
+        }
+
+        public void writeChangeListing() {
+        }
+
+	public void writeAnnotatedSourceFile() {
+	}
+}
+
