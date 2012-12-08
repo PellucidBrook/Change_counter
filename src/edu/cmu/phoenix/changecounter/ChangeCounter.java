e523751878ef7329637050fb8177b7430b34ab8f
Christine Parry <blightofyou@gmail.com>
2012-12-07 20:43:21 -0500

    Updated main function

--- src/edu/cmu/phoenix/changecounter/ChangeCounter.java
+++ src/edu/cmu/phoenix/changecounter/ChangeCounter.java
@@ -1,17 +1,49 @@
 package edu.cmu.phoenix.changecounter;
 
+import java.io.OutputStream;
+import java.util.ArrayList;
+
 public class ChangeCounter {
 
   public static void main(String [] args) {
 		
-		UserInterface ui = new UserInterface();
+	  UserInterface ui = new UserInterface();
+	  ui.executeUserInterface();
 		
-		ui.executeUserInterface();
+	  // Get information from user for program 1 (old version)
+	  
+	  // Set information in Program class instance (oldProgram)
+	  Program oldProgram = new Program();
+	  oldProgram.setVersion(ui.getVersionNum());
+	  oldProgram.setFileName(ui.getOldFilePath());
+	  
+	  // Get information from user for program 2 (new version)
+	  
+	  // Set information in Program class instance (newProgram)
+	  Program newProgram = new Program();
+	  newProgram.setVersion(ui.getVersionNum());
+	  newProgram.setFileName(ui.getNewFilePath());
+	  // Get change information from user
+	  
+	  // Send oldProgram and newProgram into ProgramComparer
+	  ProgramComparer pc = new ProgramComparer();
+	  ArrayList<ModifiedLine> = pc.compare(oldProgram, newProgram);
+	  
+	  
+		
+		
 		int versionNum = ui.getVersionNum();
 		int changeNum = ui.getChangeNum();
 		Change change = ui.getChange();
-		String oldFilePath = ui.getOldFilePath();
-		String newFilePathString = ui.getNewFilePath();
+		
+		
+		
+		OutputHandler op = new OutputHandler();
+		
+		op.writeChangeListing(Program newCL, OutputStream oStream);
+		op.writeAnnotatedSourceFile(Program newSF, OutputStream oStream);
+		
+		
 		
 	}
 	
