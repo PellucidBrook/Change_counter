6140bde2dd3a75a2e9f497d56e9f5b5386378958
Christine Parry <blightofyou@gmail.com>
2012-12-09 16:51:17 -0500

    Updated main method to match UserInterface output and Program attributes.

--- src/edu/cmu/phoenix/changecounter/ChangeCounter.java
+++ src/edu/cmu/phoenix/changecounter/ChangeCounter.java
@@ -5,67 +5,62 @@ import java.util.ArrayList;
 
 public class ChangeCounter {
 
-  public static void main(String [] args) {
-		
-	  
-	  while(true) {
-	  
-	  
-	  UserInterface userInterface = new UserInterface();
-	  userInterface.executeUserInterface();
-		
-	  int newVersionNum = userInterface.getVersionNum();
-	  int oldVersionNum = newVersionNum - 1;
-	  
-	  
-	  // Get information from user for program 1 (old version)
-	  
-	  // Set information in Program class instance (oldProgram)
-	  Program oldVersion = new Program();
-	  oldProgram.setVersion(userInterface.getVersionNum());
-	  oldProgram.setFileName(userInterface.getOldFilePath());
-	  
-	  // Get information from user for program 2 (new version)
-	  
-	  // Set information in Program class instance (newProgram)
-	  Program newVersion = new Program();
-	  newProgram.setVersion(userInterface.getVersionNum());
-	  newProgram.setFileName(userInterface.getNewFilePath());
-	  // Get change information from user
-	  
-	  // Send oldProgram and newProgram into ProgramComparer
-	  ProgramComparer pc = new ProgramComparer();
-	  ArrayList<ModifiedLine> = pc.compare(oldVersion, newVersion);
-	  
-	  
-		
-		
-		int versionNum = ui.getVersionNum();
-		int changeNum = ui.getChangeNum();
-		Change change = ui.getChange();
-		
-		
-		
-		
-		
-		
-		
-		
-//		Ask user if they want to write the output to the change listing or source file
-		OutputHandler outputHandler = new OutputHandler();
-		String outputType = ui.promptUserForOutputType();
-		if (outputType.equals("c")) {
-			outputHandler.writeChangeListing(newVersion, OutputStream oStream);
-		} else if (outputType.equals("s")) {
-			outputHandler.writeAnnotatedSourceFile(newVersion, OutputStream oStream);
-		} 
-		else {
-			System.out.println("WHAT HAVE YOU DONE? You shouldn't be here!");
+	public static void main(String [] args) {
+
+
+		while(true) {
+
+
+			UserInterface userInterface = new UserInterface();
+			userInterface.executeUserInterface();
+
+			//	  Create oldVersion and newVersion of Program class
+			Program oldProgram = new Program();
+			oldProgram.setVersion(userInterface.getOldVersion());
+			oldProgram.setFileName(userInterface.getOldFilePath());
+
+			Program newProgram = new Program();
+			newProgram.setVersion(userInterface.getNewVersion());
+			newProgram.setFileName(userInterface.getNewFilePath());
+
+			// Send oldProgram and newProgram into ProgramComparer
+			ProgramComparer pc = new ProgramComparer();
+			ArrayList<ModifiedLine> = pc.compare(oldProgram, newProgram);
+
+
+			// Get change information from user
+
+
+
+
+
+
+			int versionNum = ui.getVersionNum();
+			int changeNum = ui.getChangeNum();
+			Change change = ui.getChange();
+
+
+
+
+
+
+
+
+			//		Ask user if they want to write the output to the change listing or source file
+			OutputHandler outputHandler = new OutputHandler();
+			String outputType = ui.promptUserForOutputType();
+			if (outputType.equals("c")) {
+				outputHandler.writeChangeListing(newVersion, OutputStream oStream);
+			} else if (outputType.equals("s")) {
+				outputHandler.writeAnnotatedSourceFile(newVersion, OutputStream oStream);
+			} 
+			else {
+				System.out.println("WHAT HAVE YOU DONE? You shouldn't be here!");
+			}
+
+
 		}
-		
-		
-	  }
-		
+
 	}
-	
+
 }
